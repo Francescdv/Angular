5099349e079f68319a2b6539a901bdeadbc46703
francescdv <francescdv@gmail.com>
2021-08-10 00:37:17 +0200

    no message

--- 01-bases/src/app/app.module.ts
+++ 01-bases/src/app/app.module.ts
@@ -2,21 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ContadorComponent } from './contador/contador.component';
-import { HeroeComponent } from './heroes/heroe/heroe.component';
-import { ListadoComponent } from './heroes/listado/listado.component';
+import { HeroesModule } from './heroes/listado/heroes.module';
+import { ContadorModule } from './contador/contador.modulo';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContadorComponent,
-    HeroeComponent,
-    ListadoComponent
+
+
 
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HeroesModule,
+    ContadorModule
   ],
   providers: [],
   bootstrap: [AppComponent]

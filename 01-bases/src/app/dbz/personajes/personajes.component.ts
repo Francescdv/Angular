47c1f5aaa0ca208598a1bfc41c529deddb1f6bb9
francescdv <francescdv@gmail.com>
2021-08-12 23:26:36 +0200

    no message

---
+++ 01-bases/src/app/dbz/personajes/personajes.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Personaje } from '../interfaces/dbz.interface';
+import { DbzService } from '../services/dbz.service';
+
+
+@Component({
+  selector: 'app-personajes',
+  templateUrl: './personajes.component.html',
+
+})
+export class PersonajesComponent {
+
+  //@Input() personajes: any[] = [];
+
+  get personajes(){
+  return this.DbzService.personajes;
+  }
+
+ constructor (private DbzService:DbzService){}
+}
+

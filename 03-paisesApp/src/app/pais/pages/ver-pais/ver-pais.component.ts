e07bdecf40abcce49527707f6b68fab4c229cc51
francescdv <francescdv@gmail.com>
2021-08-20 00:21:55 +0200

    no message

--- 03-paisesApp/src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ 03-paisesApp/src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-ver-pais',
@@ -6,11 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class VerPaisComponent implements OnInit {
+export class VerPaisComponent {
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
 
 }

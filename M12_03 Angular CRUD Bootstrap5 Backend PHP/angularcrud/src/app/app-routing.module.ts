0b770b4fa8a4cb5966238f00f7957bb61fa57f89
francescdv <francescdv@gmail.com>
2021-09-30 12:48:12 +0200

    no message

---
+++ M12_03 Angular CRUD Bootstrap5 Backend PHP/angularcrud/src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { UsersComponent } from './users/users.component';
+import { AdduserComponent } from './adduser/adduser.component';
+import { UserComponent } from './user/user.component';
+
+const routes: Routes = [
+  { path: '', component: UsersComponent },
+  { path: 'users', component: UsersComponent },
+  { path: 'adduser', component: AdduserComponent },
+  { path: 'user/:id', component: UserComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
